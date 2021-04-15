692f7ccb518b9f34d0980bc534a4ee7b9306fe9c
Dmitry Chebykin <dim_c@mail.ru>
2021-04-15 18:53:35 +0700

    refactoring

--- src/main/java/Calculations.java
+++ src/main/java/Calculations.java
@@ -1,12 +1,11 @@
 public class Calculations {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    double x = 3.0 - (56 - 12) / 44.0 * (4 / 2.0);
+    double y = 2 * x / (33 - x);
+    double z = -x / (2 * y);
 
-        double x = 3.0 - (56 - 12) / 44.0 * (4 / 2.0);
-        double y = 2 * x / (33 - x);
-        double z = -x / (2 * y);
-
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-        System.out.println("z = " + z);
-    }
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+    System.out.println("z = " + z);
+  }
 }
